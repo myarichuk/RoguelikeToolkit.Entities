4dc03928595a306922dd6b8ab15b5343b4cb41ba
Michael Yarichuk <michael.yarichuk@gmail.com>
2022-09-14 13:48:59 +0300

    chore: add exceptions and template loader

---
+++ src/RoguelikeToolkit.Entities/Exceptions/FailedToParseException.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace RoguelikeToolkit.Entities.Exceptions;
+
+/// <inheritdoc />
+internal class FailedToParseException : Exception
+{
+	/// <inheritdoc />
+	[SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1648:InheritDocMustBeUsedWithInheritingClass", Justification = "Reviewed.")]
+	public FailedToParseException(string failureReason)
+		: base($"Failed to parse a template. Reason: {failureReason}")
+	{
+	}
+
+	/// <inheritdoc />
+	[SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1648:InheritDocMustBeUsedWithInheritingClass", Justification = "Reviewed.")]
+	public FailedToParseException(string templateName, string failureReason)
+		: base($"Failed to parse a template (name = {templateName}). Reason: {failureReason}")
+	{
+	}
+}
